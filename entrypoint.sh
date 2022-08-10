b69f408e451fbf955b75e61aa1bc66870c2e57f5
Atsushi Abe <piste@jp.ibm.com>
2022-08-10 09:55:51 +0900

    Remove I/F checking for the ltfs-backends repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,3 @@
 ./configure --prefix=$1 --enable-message-checker --enable-warning-as-error
 make
 make install
-
-# Backend I/F checking
-cd $1
-git clone https://github.com/LinearTapeFileSystem/ltfs-backends.git
-cd ltfs-backends
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/tmp/ltfs/lib/pkgconfig
-./autogen.sh
-./configure --enable-checkonly
-make
